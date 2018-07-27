8c26f1e170f997f5e5f0d06d1e6ef308da16bd2a
John Weisbrodt <31863920+akadubss@users.noreply.github.com>
2018-07-27 19:24:54 -0400

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+var header = document.getElementByClassName("navbar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+}); // jQuery
